bce00031639133f0cabc9d046444b71d039f2772
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2021-08-22 14:41:33 +0100

    Part 1

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (RequestInterface $request, ResponseInterface $response, array $args) {
+    $response->getBody()->write('Hello from Slim 4 request handler');
+    return $response;
+});
+
+$app->run();
